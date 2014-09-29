8eebbca8c60036d179fcc36ea3c08be99506f8b3
marciojss <marcio.sszzantana@gmail.com>
2014-09-29 15:41:34 -0300

    tentativa de correcao de erros nas views

--- cal/views.py
+++ cal/views.py
@@ -1,12 +1,14 @@
 import time
+import calendar
+from datetime import date, datetime, timedelta
+
+from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
-from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
 from django.forms.models import modelformset_factory
-from datetime import date, datetime, timedelta
-import calendar
+from django.template import RequestContext
 
 from cal.models import *
 
@@ -14,13 +16,36 @@ mnames = "January February March April May June July August September October No
 mnames = mnames.split()
 
 
+def _show_users(request):
+    """Return show_users setting; if it does not exist, initialize it."""
+    s = request.session
+    if not "show_users" in s:
+        s["show_users"] = True
+    return s["show_users"]
+
+def settings(request):
+    """Settings screen."""
+    s = request.session
+    _show_users(request)
+    if request.method == "POST":
+        s["show_users"] = (True if "show_users" in request.POST else False)
+    return render_to_response("cal/settings.html", add_csrf(request, show_users=s["show_users"]))
+
+def reminders(request):
+    """Return the list of reminders for today and tomorrow."""
+    year, month, day = time.localtime()[:3]
+    reminders = Entry.objects.filter(date__year=year, date__month=month,
+                                   date__day=day, creator=request.user.id, remind=True)
+    tomorrow = datetime.now() + timedelta(days=1)
+    year, month, day = tomorrow.timetuple()[:3]
+    return list(reminders) + list(Entry.objects.filter(date__year=year, date__month=month,
+                                   date__day=day, creator=request.user.id, remind=True))
+
 def main(request, year=None):
     """Main listing, years and months; three years per page."""
     # prev / next years
-    if year: 
-        year = int(year)
-    else:    
-        year = time.localtime()[0]
+    if year: year = int(year)
+    else:    year = time.localtime()[0]
 
     nowy, nowm = time.localtime()[:2]
     lst = []
@@ -32,7 +57,8 @@ def main(request, year=None):
             entry = current = False   # are there entry(s) for this month; current month?
             entries = Entry.objects.filter(date__year=y, date__month=n+1)
             if not _show_users(request):
-                entries = entries.filter(creator=request.user)
+                entries = entries.filter(creator=request.user.id)
+
             if entries:
                 entry = True
             if y == nowy and n+1 == nowm:
@@ -50,10 +76,8 @@ def month(request, year, month, change=None):
     # apply next / previous change
     if change in ("next", "prev"):
         now, mdelta = date(year, month, 15), timedelta(days=31)
-        if change == "next":
-            mod = mdelta
-        elif change == "prev":
-            mod = -mdelta
+        if change == "next":   mod = mdelta
+        elif change == "prev": mod = -mdelta
 
         year, month = (now+mod).timetuple()[:2]
 
@@ -70,6 +94,8 @@ def month(request, year, month, change=None):
         entries = current = False   # are there entries for this day; current day?
         if day:
             entries = Entry.objects.filter(date__year=year, date__month=month, date__day=day)
+            if not _show_users(request):
+                entries = entries.filter(creator=request.user)
             if day == nday and year == nyear and month == nmonth:
                 current = True
 
@@ -79,13 +105,16 @@ def month(request, year, month, change=None):
             week += 1
 
     return render_to_response("cal/month.html", dict(year=year, month=month, user=request.user,
-                        month_days=lst, mname=mnames[month-1]))
-
+                        month_days=lst, mname=mnames[month-1], reminders=reminders(request)))
 
 def day(request, year, month, day):
     """Entries for the day."""
     EntriesFormset = modelformset_factory(Entry, extra=1, exclude=("creator", "date"),
-                                              can_delete=True)
+                                          can_delete=True)
+    other_entries = []
+    if _show_users(request):
+        other_entries = Entry.objects.filter(date__year=year, date__month=month,
+                                       date__day=day).exclude(creator=request.user)
 
     if request.method == 'POST':
         formset = EntriesFormset(request.POST)
@@ -96,50 +125,18 @@ def day(request, year, month, day):
                 entry.creator = request.user
                 entry.date = date(int(year), int(month), int(day))
                 entry.save()
-            return HttpResponseRedirect(reverse("dbe.cal.views.month", args=(year, month)))
+            return HttpResponseRedirect(reverse("cal.views.month", args=(year, month)))
 
     else:
         # display formset for existing enties and one extra form
         formset = EntriesFormset(queryset=Entry.objects.filter(date__year=year,
             date__month=month, date__day=day, creator=request.user))
-    other_entries = []
-    if _show_users(request):
-        other_entries = Entry.objects.filter(date__year=year, date__month=month,
-                                       date__day=day).exclude(creator=request.user)
-
     return render_to_response("cal/day.html", add_csrf(request, entries=formset, year=year,
             month=month, day=day, other_entries=other_entries, reminders=reminders(request)))
 
-def add_csrf(request, ** kwargs):
+
+def add_csrf(request, **kwargs):
     """Add CSRF and user to dictionary."""
-    d = dict(user=request.user, ** kwargs)
+    d = dict(user=request.user, **kwargs)
     d.update(csrf(request))
     return d
-
-def reminders(request):
-    """Return the list of reminders for today and tomorrow."""
-    year, month, day = time.localtime()[:3]
-    # import ipdb; ipdb.set_trace()
-    reminders = Entry.objects.filter(date__year=year, date__month=month,
-                                   date__day=day, creator=request.user.id, remind=True)
-    tomorrow = datetime.now() + timedelta(days=1)
-    year, month, day = tomorrow.timetuple()[:3]
-    return list(reminders) + list(Entry.objects.filter(date__year=year, date__month=month,
-                                   date__day=day, creator=request.user.id, remind=True))
-
-
-def _show_users(request):
-    """Return show_users setting; if it does not exist, initialize it."""
-    s = request.session
-    if not "show_users" in s:
-        s["show_users"] = True
-    return s["show_users"]
-
-
-def settings(request):
-    """Settings screen."""
-    s = request.session
-    _show_users(request)
-    if request.method == "POST":
-        s["show_users"] = (True if "show_users" in request.POST else False)
-    return render_to_response("cal/settings.html", add_csrf(request, show_users=s["show_users"]))
